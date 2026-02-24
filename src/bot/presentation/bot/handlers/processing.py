30eb07f40df598fd19307799c8b75c6b5362339c
Andy Vertex <andyvertex@z>
2026-02-24 21:07:20 +0300

    wip: history

--- src/bot/presentation/bot/handlers/processing.py
+++ src/bot/presentation/bot/handlers/processing.py
@@ -1,7 +1,11 @@
 import structlog
 from aiogram import Router, types
-from dishka.integrations.aiogram import FromDishka, inject
+from dishka.integrations.aiogram import AsyncSession, FromDishka, inject
 
+from src.bot.config import Config
+from src.bot.infra.database.repositories.message_history_repository import (
+    MessageRepository,
+)
 from src.bot.infra.llm.setup import OpenRouterService
 
 ROUTER = Router()
@@ -9,10 +13,15 @@ ROUTER = Router()
 logger = structlog.get_logger()
 
 
+# todo https://openrouter.ai/docs/api/api-reference/models/get-models
+
+
 @ROUTER.message()
 @inject
 async def processing(
-    message: types.Message, service: FromDishka[OpenRouterService]
+    message: types.Message,
+    service: FromDishka[OpenRouterService],
+    session: FromDishka[AsyncSession],
 ):
     if message.text and message.text.startswith("/"):
         return
@@ -22,7 +31,10 @@ async def processing(
         )
         logger.error("Error", error="Failed to receive message")
         return
-
+    message_repository = MessageRepository(
+        config=Config(), session=session, user_id=message.from_user.id
+    )
+    await message_repository.add_message(message.text)
     answer = await message.answer("Начинаю обработку...")
     response = await service.llm_answer(message.text)
     await answer.edit_text(response)
